1ec8ffee5ac3401538c716a08f4914ceb5593c82
tangvy <949129882@qq.com>
2021-03-28 10:15:02 +0800

    网关基础配置

---
+++ cloud-gateway-gateway8010/src/main/java/com/tangv/cloud/GatewayMain8010.java
@@ -0,0 +1,17 @@
+package com.tangv.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author:   tangwei
+ * Date:     2021/3/28 9:52
+ * Description:
+ */
+@SpringBootApplication
+public class GatewayMain8010 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain8010.class, args);
+    }
+}
