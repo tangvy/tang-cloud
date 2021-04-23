ccdee6601cc6e3bab37e3eb795b93c1e8bc8e5ab
tangvy <949129882@qq.com>
2021-04-24 00:35:43 +0800

    nacos配置nginx转发

--- cloud-alibaba-consumer-order8081/src/main/java/com/tangv/cloud/controller/OrderController.java
+++ cloud-alibaba-consumer-order8081/src/main/java/com/tangv/cloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.tangv.cloud.controller;
 
 import com.tangv.cloud.entities.CommonResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import javax.annotation.Resource;
  * Description:
  */
 @RestController
+@RefreshScope
 public class OrderController {
 
     @Resource
@@ -26,9 +28,17 @@ public class OrderController {
     @Value("${service-url.nacos-user-service}")
     private String paymentService;
 
+    @Value("${name}")
+    private String name;
+
     @GetMapping("/nacos/consumer/get/{id}")
     public CommonResult<Integer> get(@PathVariable Long id) {
         return restTemplate.getForObject(paymentService + "/nacos/provider/get/" + id,CommonResult.class);
+    }
+
+    @GetMapping("/get")
+    public CommonResult<String> getName() {
+        return new CommonResult(200, name);
     }
 
 }
