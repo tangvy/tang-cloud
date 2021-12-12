102fbdbeb65b4c9ca8a104e200827636485cf9fe
tangvy <949129882@qq.com>
2021-12-12 11:59:31 +0800

    update .gitignore

---
+++ cloud-consumer-order80/src/main/java/com/tangv/cloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.tangv.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Description:
+ * Author:      TangWei
+ * Date:        2020/7/9 9:28 下午
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/tangv/cloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.tangv.cloud.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Description:
+ * Author:      TangWei
+ * Date:        2020/7/9 9:47 下午
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
